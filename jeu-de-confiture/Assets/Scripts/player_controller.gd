93192cc742d0a528a7603c2bf8bdd4c49130856c
JulesMEI <jules.meignal@epitech.eu>
2026-02-07 13:05:28 +0100

    feat: updated player, separated infos, added dash, beginning of roll implemented but WIP

--- jeu-de-confiture/Assets/Scripts/player_controller.gd
+++ jeu-de-confiture/Assets/Scripts/player_controller.gd
@@ -2,36 +2,81 @@ extends CharacterBody2D
 
 class_name PlayerController
 
+@export var animated_sprite : AnimatedSprite2D
+
 @export var speed = 10.0
 @export var jump_vel = 10.0
 
 var speed_mul = 30.0
 var slow_mul = 0.05
 var jump_mul = -30.0
+
+var dashed = 0
+#var rolling = 0
+
+@export var jump_cost = 50
+@export var dash_cost = 50
+
 var direction = 0
+var looking_at = 1
 
 # const SPEED = 300.0
 # const JUMP_VELOCITY = -400.0
 
+func jump(remaining_time : float = 100.0):
+	if jump_cost < remaining_time and Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_vel * jump_mul
+#
+#func roll(remaining_time : float = 100.0) -> bool :
+	#if not Input.is_action_pressed("roll"):
+		#return false
+	#rolling = true
+	#return true
+
+# Makes the player move and allows him to roll to 
+func move():
+	if direction:
+		# quicker movement on the ground
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, direction * speed * speed_mul, \
+				speed * 5)
+			return
+
+		# slower turn in the air
+		velocity.x = move_toward(velocity.x, direction * speed * speed_mul, \
+			speed)
+
+	elif is_on_floor():
+		# rolling preserves momentum
+		#if roll():
+			#return
+		# staying on the ground loses it
+		velocity.x = move_toward(velocity.x, 0, speed * speed_mul * slow_mul)
+
+# Makes the player dash and gives him some upward velocity and more angular momentum
+func dash(remaining_time : float = 100.0):
+	if !Input.is_action_just_pressed("dash") or dashed:
+		return
+	dashed = true
+	velocity.x += speed * speed_mul * looking_at 
+	velocity.y = jump_vel * jump_mul / 2
+
+# Physics process
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		# rolling = false
+		dashed = 0
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_vel * jump_mul
+	if direction: looking_at = direction
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	jump()
+
 	direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		if is_on_floor():
-			velocity.x = move_toward(velocity.x, direction * speed * speed_mul, \
-				speed * 5)
-		else :
-			velocity.x = move_toward(velocity.x, direction * speed * speed_mul, \
-				speed)
-	elif is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, speed * speed_mul * slow_mul)
+	move()
+
+	dash()
 
 	move_and_slide()
