f6afed29e7cbc08dc7c8f17ae24f147aa1acd176
JulesMEI <jules.meignal@epitech.eu>
2026-02-16 17:25:17 +0100

    fix: music player AGAIN!!!

--- jeu-de-confiture/Assets/Scripts/music_player.gd
+++ jeu-de-confiture/Assets/Scripts/music_player.gd
@@ -3,12 +3,4 @@ extends AudioStreamPlayer
 const music = preload("res://Sound/Music/Pu-Li-Ru-La_spotdown.org.mp3")
 
 func _ready():
-	_play_music()
-
-func _play_music(volume = -15.0):
-	if (stream == music):
-		return
-	stream = music
-	volume_db = volume
-	play()
 	finished.connect(func(): play(0))
